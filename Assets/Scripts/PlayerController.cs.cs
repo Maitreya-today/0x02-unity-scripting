c9a58d40f1ee3ed47f103bacb9fc6b57abe50db3
Maitreya-today <lessismoreclint@gmail.com>
2023-10-08 23:46:29 -0500

    WASD

--- Assets/Scripts/PlayerController.cs.cs
+++ Assets/Scripts/PlayerController.cs.cs
@@ -1,27 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f; // Adjustable speed value in the Inspector
+    public float speed = 5f;
+    private int score = 0;
+    public int health = 5;
 
-    // FixedUpdate is used for physics-related updates
-    void FixedUpdate()
+    private Rigidbody playerRigidBody;
+    private void Start()
     {
-        float horizontalInput = Input.GetAxis("Horizontal"); // A/D or left/right arrow keys
-        float verticalInput = Input.GetAxis("Vertical");     // W/S or up/down arrow keys
-
-        // Calculate the movement vector
-        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput) * speed * Time.fixedDeltaTime;
-
-        // Apply the movement to the player's position
-        transform.Translate(movement);
+        playerRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        // You can add other non-physics-related updates here if needed
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        playerRigidBody.velocity = movement * speed;
     }
-}
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            score++;
+            Debug.Log("Score: " + score);
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Trap"))
+        {
+            health--;
+            Debug.Log("Health: " + health);
+
+            if (health <= 0)
+            {
+                GameOver();
+            }
+        }
+        else if (other.CompareTag("Goal"))
+        {
+            Debug.Log("You win!");
+        }
+    }
+    private void Update()
+    {
+        if (health <=0 )
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("Game Over!");
+        score = 0;
+        health = 5;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
